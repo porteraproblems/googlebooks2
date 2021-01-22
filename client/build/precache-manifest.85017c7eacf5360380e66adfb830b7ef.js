a2c3713d235c95b95d8ad7467b87af9763fb906b
Alexander Portera <aportera3@gmail.com>
2021-01-21 22:42:58 -0500

    commit

---
+++ client/build/precache-manifest.85017c7eacf5360380e66adfb830b7ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f4d185d9dd9ef8c73c14049ba6f7d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c7f32b4013898d3fcec",
+    "url": "/static/css/main.33e6fd29.chunk.css"
+  },
+  {
+    "revision": "b54b35988f847c8fb4de",
+    "url": "/static/js/2.7bf1cf89.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.7bf1cf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c7f32b4013898d3fcec",
+    "url": "/static/js/main.2598f604.chunk.js"
+  },
+  {
+    "revision": "816cf679577cc3650d0c",
+    "url": "/static/js/runtime-main.87c4f50d.js"
+  }
+]);
